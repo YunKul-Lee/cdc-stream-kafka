7734254c943c375d5a1d2658b56d665db688e104
jake <gerion@gmail.com>
2024-01-23 10:50:26 +0900

    Dto, json deserializer 정의

---
+++ src/main/java/com/jake/cdc/config/jackson/OperationTypeDeserializer.java
@@ -0,0 +1,17 @@
+package com.jake.cdc.config.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.jake.cdc.dto.DebeziumEventDto;
+
+
+import java.io.IOException;
+
+public class OperationTypeDeserializer extends JsonDeserializer<DebeziumEventDto.OperationType> {
+
+    @Override
+    public DebeziumEventDto.OperationType deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        return DebeziumEventDto.OperationType.fromCode(jsonParser.getValueAsString());
+    }
+}
